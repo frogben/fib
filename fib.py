ce114a60245af990438f590dc0860861b62c4b3f
FrogBen <s71351015@gmail.com>
2018-12-26 20:05:35 +0800

    push2

--- fib.py
+++ fib.py
@@ -1,19 +1,16 @@
 
 # coding: utf-8
+import time
 
-# In[1]:
-
-
-from functools  import lru_cache
 def memoize(f):
-    cache = {}
-    def decorated_function(*args):
-        if args in cache:
-            return cache[args]
+    cache_list = {}
+    def add_memoize(*args):
+        if args in cache_list:
+            return cache_list[args]
         else:
-            cache[args] = f(*args)
-            return cache[args]
-    return decorated_function
+            cache_list[args] = f(*args)
+            return cache_list[args]
+    return add_memoize
 
 def fib_o(n):
     return n if n < 2 else fib_o(n-2) + fib_o(n-1)
@@ -22,35 +19,17 @@ def fib_o(n):
 def fib(n):
     return n if n < 2 else fib(n-2) + fib(n-1)
 
-@lru_cache()
-def fib_l(n):
-    return n if n < 2 else fib_l(n-2) + fib_l(n-1)
 
-
-# In[3]:
-
-
-import time
 tStart = time.time()
+print('origin fib(35):')
 print(fib_o(35))
 tEnd = time.time()
 print("It cost %f sec" % (tEnd - tStart))
 
-
-# In[4]:
-
-
 tStart = time.time()
+print('with cache fib(35):')
 print(fib(35))
 tEnd = time.time()
 print("It cost %f sec" % (tEnd - tStart))
 
-
-# In[5]:
-
-
-tStart = time.time()
-print(fib_l(35))
-tEnd = time.time()
-print("It cost %f sec" % (tEnd - tStart))
 
