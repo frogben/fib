66b165246581cb030f826e643f5cfdf02e86377c
FrogBen <s71351015@gmail.com>
2018-12-26 19:54:12 +0800

    push

--- fib.py
+++ fib.py
@@ -4,59 +4,53 @@
 # In[1]:
 
 
-import time
-import sys
-# sys.setrecursionlimit(1000000)
+from functools  import lru_cache
+def memoize(f):
+    cache = {}
+    def decorated_function(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = f(*args)
+            return cache[args]
+    return decorated_function
+
 def fib_o(n):
-    if n < 2:
-        return n
-    else:
-        return fib_o(n-1) + fib_o(n-2)
+    return n if n < 2 else fib_o(n-2) + fib_o(n-1)
+
+@memoize
+def fib(n):
+    return n if n < 2 else fib(n-2) + fib(n-1)
+
+@lru_cache()
+def fib_l(n):
+    return n if n < 2 else fib_l(n-2) + fib_l(n-1)
 
 
+# In[3]:
 
-# In[2]:
 
-
+import time
 tStart = time.time()
 print(fib_o(35))
 tEnd = time.time()
 print("It cost %f sec" % (tEnd - tStart))
 
 
-# In[3]:
-
-
-cache = {} 
-# def cache_find(n):
-#     if n not in cache.keys():
-#         cache[n] = fib(n)
-#         print(cache)
-#     return cache[n]
-
-def use_cache(func):
-    def wrapper(n):
-        if n not in cache.keys():
-            cache[n] = fib_o(n)
-            # print(cache)
-        return cache[n]
-    return wrapper
-
-@use_cache
-def fib(n):
-    if n < 2:
-        return n
-    else:
-        return fib(n-1) + fib(n-2)
-
-
-
-
-# In[ ]:
+# In[4]:
 
 
 tStart = time.time()
 print(fib(35))
+tEnd = time.time()
+print("It cost %f sec" % (tEnd - tStart))
+
+
+# In[5]:
+
+
+tStart = time.time()
+print(fib_l(35))
 tEnd = time.time()
 print("It cost %f sec" % (tEnd - tStart))
 
